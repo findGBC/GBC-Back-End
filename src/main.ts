6a68d66167d87ac47076886e7443a0f6bad73e0c
BohSolo <bohdan.solomakha@cowchain.io>
2024-04-25 17:03:26 +0300

    fix[gbc-6]: add proccess.env.PORT

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () =>
-    console.log(`Server successfully started on port 3000`),
+  await app.listen(process.env.PORT ? +process.env.PORT : 5001, () =>
+    console.log(`Server successfully started on port 5001`),
   );
   //const nftService = new NftService()
   //const nftController = new NftController(nftService);
