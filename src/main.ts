6e25c0413b0a66eac97a1381dd8f234fe89d59f5
BohSolo <bohdan.solomakha@cowchain.io>
2024-04-25 16:51:55 +0300

    fix[gbc-6]: delete nftService from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {NftService} from "./modules/nft/nft.service";
-import {NftController} from "./modules/nft/nft.controller";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  const nftService = new NftService()
-  const nftController = new NftController(nftService);
+  await app.listen(3000, () =>
+    console.log(`Server successfully started on port 3000`),
+  );
+  //const nftService = new NftService()
+  //const nftController = new NftController(nftService);
   //nftController.generateNftByAllIds()
 }
 bootstrap();
